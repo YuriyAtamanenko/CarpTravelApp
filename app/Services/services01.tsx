effc96eea6ef052b6863b7a9b7362a96ec0a093d
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-12-16 20:54:44 +0200

    added services images and added servuces markup

--- app/Services/services01.tsx
+++ app/Services/services01.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import ImgServices01 from "../../public/ImgServices01.jpg";
+
 export default function Services01() {
   return (
-    <section className="services section" id="services">
-      <div className="container mx-auto max-w-7xl outline">01</div>
+    <section className="services01 section py-20" id="services">
+      <div className="container h-full mx-auto max-w-7xl p-6">
+        <h2>WE OFFER</h2>
+        <Image src={ImgServices01} alt="ATVs Traveling" />
+        <span>01/05</span>
+        <ul>
+          <li>ATVs Traveling</li>
+          <li>Rock climbing</li>
+          <li>Hot air ballooning</li>
+          <li>Skydiving</li>
+          <li>Rafting</li>
+        </ul>
+        <p>
+          Join exciting rafting expeditions on the waterways of the Carpathians.
+          Go through challenging waterways and overcome gusty waves, feel the
+          adrenaline, and enjoy the incredible views of the surrounding
+          mountains.
+        </p>
+      </div>
     </section>
   );
 }

ed4cba8b797479a6da4ae206528de58caa87654e
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-12-18 18:21:37 +0200

    some changes

--- app/Services/page.tsx
+++ app/Services/page.tsx
@@ -1,6 +1,14 @@
 "use client";
+import { Navigation } from "swiper/modules";
+
 import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade } from "swiper/modules";
+import { useSwiperSlide } from "swiper/react";
+// Import Swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+
 import Services01 from "./services01";
 import Services02 from "./services02";
 import Services03 from "./services03";
@@ -8,11 +16,21 @@ import Services04 from "./services04";
 import Services05 from "./services05";
 
 export default function Services() {
+  const swiperSlide = useSwiperSlide();
+  // const GoTo = (index: number) => {
+  //   console.log("GO");
+  //   swiper.slideTo(index);
+  // };
+
+  console.dir(swiperSlide);
+
   return (
     <Swiper
+      modules={[Navigation, EffectFade]}
       spaceBetween={50}
       slidesPerView={1}
-      onSwiper={(swiper) => console.log(swiper)}
+      navigation
+      effect="fade"
     >
       <SwiperSlide>
         <Services01 />

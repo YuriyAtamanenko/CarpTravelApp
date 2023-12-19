20c07e7980ffa31496f0c36d12ed8c9fd5393d6e
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-12-19 13:28:14 +0200

    Add Career section markup

--- app/Career/page.tsx
+++ app/Career/page.tsx
@@ -1,7 +1,51 @@
 export default function Career() {
   return (
     <section className="career section" id="career">
-      <div className="mx-auto max-w-7xl outline"> Career Section</div>
+      <div className="flex before:mx-auto max-w-7xl outline">
+        <div className="w-1/2">
+          <h2>CHOOSE US</h2>
+          <p>Why us ?</p>
+          <ul>
+            <li>
+              <h3>Professional development</h3>
+              <p>
+                We offer growth opportunities and a creative environment to
+                nurture your talents.
+              </p>
+            </li>
+            <li>
+              <h3>Teamwork</h3>
+              <p>
+                Join our close-knit family, where support and inspiration
+                abound.
+              </p>
+            </li>
+            <li>
+              <h3>Stimulating work environment</h3>
+              <p>
+                Flexibility and remote options for a comfortable experience.
+              </p>
+            </li>
+            <li>
+              <h3>Exciting challenges</h3>
+              <p>
+                Unleash your potential through unforgettable projects showcasing
+                Carpathian beauty.
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="w-1/2">
+          <p>
+            Your chance to join our passionate team in Carpathian tourism.
+            Seeking talented professionals to share our common mission.
+          </p>
+          <p>
+            Don&apos;t miss your opportunity! Fill out the form right now and
+            join our team!
+          </p>
+        </div>
+      </div>
     </section>
   );
 }

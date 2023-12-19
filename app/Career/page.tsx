f20792d7de0d0dec41b357b0d78ed7270310dec6
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-12-19 14:34:03 +0200

    add Career section styles

--- app/Career/page.tsx
+++ app/Career/page.tsx
@@ -1,34 +1,44 @@
 export default function Career() {
   return (
-    <section className="career section" id="career">
-      <div className="flex before:mx-auto max-w-7xl outline">
-        <div className="w-1/2">
-          <h2>CHOOSE US</h2>
-          <p>Why us ?</p>
-          <ul>
-            <li>
-              <h3>Professional development</h3>
-              <p>
+    <section className="career section py-20" id="career">
+      <div className="flex container h-full mx-auto max-w-7xl p-6">
+        <div className="w-1/2 pr-3">
+          <h2 className="text-8xl font-thin uppercase leading-tight tracking-[-3.92px]">
+            CHOOSE <span className="font-medium">US</span>
+          </h2>
+          <p className="mt-6 ml-32 uppercase text-4xl font-extralight">
+            Why us ?
+          </p>
+          <ul className="mt-11 flex flex-col  gap-6">
+            <li className="flex  justify-end">
+              <h3 className="text-lg font-normal leading-6">
+                Professional development
+              </h3>
+              <p className="text-xs font-extralight leading-6 w-72 ml-6">
                 We offer growth opportunities and a creative environment to
                 nurture your talents.
               </p>
             </li>
-            <li>
-              <h3>Teamwork</h3>
-              <p>
+            <li className="flex  justify-end">
+              <h3 className="text-lg font-normal leading-6">Teamwork</h3>
+              <p className="text-xs font-extralight leading-6 w-72 ml-6">
                 Join our close-knit family, where support and inspiration
                 abound.
               </p>
             </li>
-            <li>
-              <h3>Stimulating work environment</h3>
-              <p>
+            <li className="flex  justify-end">
+              <h3 className="text-lg font-normal leading-6">
+                Stimulating work environment
+              </h3>
+              <p className="text-xs font-extralight leading-6 w-72 ml-6">
                 Flexibility and remote options for a comfortable experience.
               </p>
             </li>
-            <li>
-              <h3>Exciting challenges</h3>
-              <p>
+            <li className="flex  justify-end">
+              <h3 className="text-lg font-normal leading-6">
+                Exciting challenges
+              </h3>
+              <p className="text-xs font-extralight leading-6 w-72 ml-6">
                 Unleash your potential through unforgettable projects showcasing
                 Carpathian beauty.
               </p>
@@ -36,11 +46,11 @@ export default function Career() {
           </ul>
         </div>
         <div className="w-1/2">
-          <p>
+          <p className="mt-4 ml-auto text-lg font-extralight leading-6 w-72 text-justify">
             Your chance to join our passionate team in Carpathian tourism.
             Seeking talented professionals to share our common mission.
           </p>
-          <p>
+          <p className="mt-8 text-lg font-extralight leading-6 w-60">
             Don&apos;t miss your opportunity! Fill out the form right now and
             join our team!
           </p>

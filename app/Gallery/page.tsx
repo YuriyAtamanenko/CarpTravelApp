2e5a04929acb9187146abf836efe81552f48e572
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-12-23 14:18:21 +0200

    add swiper on Gallery section

--- app/Gallery/page.tsx
+++ app/Gallery/page.tsx
@@ -1,7 +1,56 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useSwiper } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import Image from "next/image";
+import Gallary01 from "../../public/Gallary/01.jpg";
+import Gallary02 from "../../public/Gallary/02.jpg";
+import Gallary03 from "../../public/Gallary/03.jpg";
+
 export default function Gallery() {
+  const swiper = useSwiper();
   return (
-    <section className="gallery section" id="gallery">
-      <div className="mx-auto max-w-7xl outline"> Gallery Section</div>
+    <section className="gallery section  py-20" id="gallery">
+      <div className="flex container h-full mx-auto max-w-7xl p-6">
+        {/* <h2 className="text-8xl font-thin uppercase leading-tight tracking-[-3.92px]">
+          OUR <span className="font-medium">GALLERY</span>
+        </h2> */}
+        {/* <div className="block"> */}
+        <Swiper
+          modules={[Navigation]}
+          rewind={true}
+          navigation={true}
+          slidesPerView={2}
+        >
+          <SwiperSlide>
+            <Image
+              src={Gallary01}
+              alt="Mountain landscape"
+              className="w-[670px]"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={Gallary02}
+              alt="Mountain landscape"
+              className="w-[670px]"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={Gallary03}
+              alt="Mountain landscape"
+              className="w-[670px]"
+            />
+          </SwiperSlide>
+        </Swiper>
+        {/* <button onClick={() => swiper.slidePrev()}>Back</button>
+          <button onClick={() => swiper.slideNext()}>Next</button> */}
+      </div>
+      {/* </div> */}
     </section>
   );
 }

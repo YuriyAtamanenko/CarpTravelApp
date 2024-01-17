a4716f4fe350ffa44b538e3285c74ee75af89b5d
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2024-01-17 21:23:32 +0200

    Update page.tsx

--- app/Gallery/page.tsx
+++ app/Gallery/page.tsx
@@ -14,43 +14,43 @@ export default function Gallery() {
   const swiper = useSwiper();
   return (
     <section className="gallery section  py-20" id="gallery">
-      <div className="flex container h-full mx-auto max-w-7xl p-6">
-        {/* <h2 className="text-8xl font-thin uppercase leading-tight tracking-[-3.92px]">
+      <div className="flex flex-col container h-full mx-auto max-w-7xl p-6">
+        <h2 className="text-8xl font-thin uppercase leading-tight tracking-[-3.92px]">
           OUR <span className="font-medium">GALLERY</span>
-        </h2> */}
-        {/* <div className="block"> */}
-        <Swiper
-          modules={[Navigation]}
-          rewind={true}
-          navigation={true}
-          slidesPerView={2}
-        >
-          <SwiperSlide>
-            <Image
-              src={Gallary01}
-              alt="Mountain landscape"
-              className="w-[670px]"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image
-              src={Gallary02}
-              alt="Mountain landscape"
-              className="w-[670px]"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image
-              src={Gallary03}
-              alt="Mountain landscape"
-              className="w-[670px]"
-            />
-          </SwiperSlide>
-        </Swiper>
-        {/* <button onClick={() => swiper.slidePrev()}>Back</button>
-          <button onClick={() => swiper.slideNext()}>Next</button> */}
+        </h2>
+        <div className="mt-6">
+          <Swiper
+            modules={[Navigation]}
+            rewind={true}
+            slidesPerView={1}
+            className="flex w-[670px]"
+          >
+            <SwiperSlide>
+              <Image
+                src={Gallary01}
+                alt="Mountain landscape"
+                className="w-[670px]"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                src={Gallary02}
+                alt="Mountain landscape"
+                className="w-[670px]"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image
+                src={Gallary03}
+                alt="Mountain landscape"
+                className="w-[670px]"
+              />
+            </SwiperSlide>
+          </Swiper>
+          <button onClick={() => swiper.slidePrev()}>Back</button>
+          <button onClick={() => swiper.slideNext()}>Next</button>
+        </div>
       </div>
-      {/* </div> */}
     </section>
   );
 }

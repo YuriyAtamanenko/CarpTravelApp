ea3e0e61d24d5d91cd8f803640d5a129e14cf3c9
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2024-01-20 17:08:10 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           boxShadow:
             "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset",
         }}
-        component={<p style={{ color: "black" }}>UP</p>}
+        component={<p style={{ color: "white" }}>UP</p>}
       />
       <Hero />
       <About />
